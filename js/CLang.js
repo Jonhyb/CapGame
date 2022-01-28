8fffea3640cc5c1114c9bba2a3b75033b95fdeab
Joao Costa <joaorcosta1991@gmail.com>
2022-01-28 13:10:00 +0000

    test

---
+++ js/CLang.js
@@ -0,0 +1,22 @@
+var TEXT_PRELOADER_CONTINUE = "BORA LÁ, BORA LÁ";
+var TEXT_GAMEOVER = "SE FODEU";
+var TEXT_SCORE = "RECORDE";
+var TEXT_BEST_SCORE = "O MELHOR!";
+var TEXT_HELP1_PC = "Use arrow keys to drive your starship. Cross the screen from side to side to avoid blocks";
+var TEXT_HELP1_MOBILE = "TOUCH THE LEFT OR RIGHT SIDE TO MOVE THE STARSHIP. Cross the screen from side to side to avoid blocks";
+var TEXT_PAUSE = "GANDA PAUSA";
+var TEXT_ARE_SURE = "WTF?";
+var TEXT_DEVELOPED = "DEVELOPED BY";
+//var TEXT_MOVE = "MEXE-TE";
+//var TEXT_EAT = "COME";
+//var TEXT_AVOID = "FOGE";
+var TEXT_MOVE = "LIE";
+var TEXT_EAT = "CHEAT";
+var TEXT_AVOID = "STEAL";
+
+var TEXT_SHARE_IMAGE = "200x200.jpg";
+var TEXT_SHARE_TITLE = "És um Eugénio!";
+var TEXT_SHARE_MSG1 = "Só fizeste <strong>";
+var TEXT_SHARE_MSG2 = " pontos.</strong>!<br><br>Pareces o Ilidio!";
+var TEXT_SHARE_SHARE1 = "My score is ";
+var TEXT_SHARE_SHARE2 = " points! Can you do better?";
